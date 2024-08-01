81c75598d802acb1c398f5702f41fb5d68af17ae
Mohamed Sherif <145474950+mohammed-sherif34@users.noreply.github.com>
2024-08-01 19:06:21 +0300

    Added project files

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'home_page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomePage.name: (context) => HomePage(),
+      },
+      initialRoute: HomePage.name,
+    );
+  }
+}
